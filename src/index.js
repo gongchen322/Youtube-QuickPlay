71bee8524355953e967bf57bf77f0c172178efa4
gongchen322 <gong27@purdue.edu>
2016-03-31 01:05:18 -0700

    Project done- version 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
-const App = () => {
-	return <div> Hi! </div>;
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+//Define API Key for Youtube
+const API_KEY = 'AIzaSyARGq4isX4kxUq83zwWmDnaTcDsPkU0qwc';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+
+		this.videoSearch('Faker');
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term},  (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo:videos[0]
+			});
+		});
+	}
+
+
+	render() {
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)},300);
+
+		return (
+			<div>
+				<SearchBar onSearchTermChange={videoSearch}/>
+				<VideoDetail video = {this.state.selectedVideo}/>
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos = {this.state.videos}/>
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
